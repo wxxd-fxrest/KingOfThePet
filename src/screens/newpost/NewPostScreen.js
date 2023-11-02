529bf1679e8e9d10f317e79678b69e1330f30eaf
fxrest704 <fxrest704@gmail.com>
2023-11-03 03:28:55 +0900

    이미지 업로드 구현 중

--- src/screens/newpost/NewPostScreen.js
+++ src/screens/newpost/NewPostScreen.js
@@ -5,57 +5,76 @@ import { Ionicons } from '@expo/vector-icons';
 import CheckBox from '@react-native-community/checkbox';
 import * as ImagePicker from 'expo-image-picker';
 import { useCreatePostMutation } from '../../store/apiSlice';
+import axios from 'axios';
+import FormData from 'form-data';
 
 const { width: SCREENWIDTH, height: SCREENHEIGHT } = Dimensions.get('window');
 
 const NewPostScreen = () => {
     const [newPost, setNewPost] = useState('');
     const [toggleCheckBox, setToggleCheckBox] = useState(false);
-
-    // const [loading, setLoading] = useState(false);
-    // const [imageUrl, setImageUrl] = useState('');
-    // const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
-    // const [saveImgUrl, setSaveImgUrl] = useState('');
-
-    // const handleImagePick = async () => {
-    //     if (!status?.granted) {
-    //         const permission = await requestPermission();
-    //         if (!permission.granted) {
-    //             // 권한이 거부된 경우에 대한 처리 로직
-    //             console.log("권한이 거부되었습니다.");
-    //             return;
-    //         }
-    //     }
-
-    //     // 이미지 선택 로직
-    //     const result = await ImagePicker.launchImageLibraryAsync({
-    //         mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    //         allowsEditing: false,
-    //         quality: 1,
-    //         aspect: [1, 1],
-    //     });
-
-    //     // console.log('result', result);
-    //     setImageUrl(result);
-    // };
-    const [image, setImage] = useState(null);
-
     const [createPost, { data, isLoading, error }] = useCreatePostMutation();
 
-    const pickImage = async () => {
+    const [imageUrl, setImageUrl] = useState('');
+    const [status, requestPermission] =
+        ImagePicker.useMediaLibraryPermissions();
+
+    const uploadImage = async () => {
+        // 권한 확인 코드: 권한이 없을 경우 물어보고, 승인하지 않을 경우 코드를 종료함
+        if (!status?.granted) {
+            const premission = await requestPermission();
+            if (!premission.granted) {
+                return null;
+            }
+        }
+
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: false,
-            aspect: [4, 3],
             quality: 1,
+            aspect: [1, 1],
         });
 
-        if (!result.canceled) {
-            setImage(result.assets[0].uri);
-            // 이미지를 백엔드로 업로드
-            // console.log(result.assets[0].uri)
+        if (result.canceled) {
+            return null; //이미지 업로드를 취소한 경우
         }
+
+        console.log(result);
+        setImageUrl(result.assets[0].uri);
+        // 이미지 업로드 결과 및 이미지 경로 업데이트
+
+        // 서버에 요청
+        const localUri = result.assets[0].uri;
+        const filename = localUri.split('/').pop();
+        const match = /\.(\w+)$/.exec(filename ?? '');
+        const type = match ? `image/${match[1]}` : `image`;
+        const formData = new FormData();
+        formData.append('profile_img', { uri: localUri, name: filename, type });
+
+        await axios({
+            method: 'post',
+            url: 'http://localhost:3000/posts/upload',
+            headers: {
+                'content-type': 'multipart/form-data',
+            },
+            data: formData,
+        });
     };
+
+    // const pickImage = async () => {
+    //     const result = await ImagePicker.launchImageLibraryAsync({
+    //         mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    //         allowsEditing: false,
+    //         aspect: [4, 3],
+    //         quality: 1,
+    //     });
+
+    //     if (!result.canceled) {
+    //         setImage(result.assets[0].uri);
+    //         // 이미지를 백엔드로 업로드
+    //         console.log(result.assets[0].uri);
+    //     }
+    // };
     // 이미지 주소 받아오는 것 까지 완료, 서버 및 데이터베이스에 업로드 하는 방법 찾아봐야 함
 
     //   const uploadImage = async (imageUri) => {
@@ -92,7 +111,7 @@ const NewPostScreen = () => {
                     onChangeText={(text) => setNewPost(text)}
                 />
                 <NewPostFooterBox>
-                    <ImgSelectIconBox onPress={pickImage}>
+                    <ImgSelectIconBox onPress={uploadImage}>
                         <Ionicons name="ios-camera" size={28} color="#6b8a47" />
                     </ImgSelectIconBox>
                 </NewPostFooterBox>
